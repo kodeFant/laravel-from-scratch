7f7f4056be4f7ca471057c4010c8ade36f9bdbd4
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-06-16 09:20:37 +0200

    Part 4 Finished

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-      <h1>Welcome to Laravel</h1>
+@section('content')
+    <div class="jumbotron text-center">
+      <h1>{{$title}}</h1>
       <p>This is the laravel application from the "Laravel from Scratch" Youtube series</p>
-    </body>
-</html>
+      <p>
+        <a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>
+        <a href="/register" class="btn btn-success btn-lg" role="button">Register</a>
+      </p>
+    </div>
+@endsection
